e53812fbc298fb1bb882e746b3939154361e26b3
Dale Flamm <dale.flamm@gmail.com>
2016-01-02 11:55:59 -0700

    Shower Time Andvanced Timer Layout complete

---
+++ app/src/main/java/com/cs7910/dale/showertime/TimerControl.java
@@ -0,0 +1,13 @@
+package com.cs7910.dale.showertime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimerControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_control);
+    }
+}
